827689dc2bb20180121f447b38d6f82d660df3bc
Raman Prasad <raman_prasad@harvard.edu>
2014-07-23 21:37:22 -0400

    It's working at a basic level. Next:
    Ajax the classification requests

--- geoconnect/classification/urls.py
+++ geoconnect/classification/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('classification.views',
-    url(r'^classify-layer/(?P<import_success_md5>\w{32})$', 'view_classify_layer_form', name="view_classify_layer_form"),
+    url(r'^classify-layer/(?P<shapefile_md5>\w{32})/(?P<import_success_md5>\w{32})$', 'view_classify_layer_form', name="view_classify_layer_form"),
 
 )
--- geoconnect/classification/views.py
+++ geoconnect/classification/views.py
@@ -3,11 +3,15 @@ import requests
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
 from worldmap_import.models import WorldMapImportAttempt, WorldMapImportFail, WorldMapImportSuccess
 from classification.forms import ClassifyLayerForm
 
-def view_classify_layer_form(request, import_success_md5):
+def view_classify_layer_form(request, shapefile_md5, import_success_md5):
+    if not (shapefile_md5 and import_success_md5):
+        raise Http404('view_classify_layer_form: missing params')
+
     if not request.POST:
         return HttpResponse('should be a POST!')
         
@@ -18,11 +22,9 @@ def view_classify_layer_form(request, import_success_md5):
     
     classify_form = ClassifyLayerForm(request.POST, **dict(import_success_object=import_success_object))
     
-    
-                                 
+                        
     if not classify_form.is_valid():
         return HttpResponse('invalid form')
-
 
     classify_params = classify_form.get_params_dict_for_classification()
     if classify_params is None:
@@ -32,8 +34,15 @@ def view_classify_layer_form(request, import_success_md5):
     classify_url = classify_form.get_worldmap_classify_api_url()
     
     req = requests.post(classify_url, data=classify_params, timeout=2)
+    if req.status_code == 200:
+        lnk = reverse('view_shapefile'\
+                        , kwargs=dict(shp_md5=shapefile_md5)\
+                        )
+        return HttpResponseRedirect(lnk)
+        
     print (req.status_code)
     print (req.text)
+    return HttpResponse('Fail out: %s<pre>%s</pre>' % (req.status_code, req.text))
     #wm_response_dict = req.json() #print()
     #print(wm_response_dict)
     return HttpResponse('post: %s<br />%s' % (request.POST.keys(), classify_url))
