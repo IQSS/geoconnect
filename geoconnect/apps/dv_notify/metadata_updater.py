b18f23adba22641910e19d056cf3e3a97477540a
Raman Prasad <raman_prasad@harvard.edu>
2014-09-18 16:16:28 -0400

    update trace

--- geoconnect/apps/dv_notify/metadata_updater.py
+++ geoconnect/apps/dv_notify/metadata_updater.py
@@ -69,10 +69,14 @@ class MetadataUpdater:
         print('2) passed key check')
         try:
             print ('params to send: %s' % dv_metadata_params)
+            print ('update url: %s' % self.api_update_url)
             req = requests.post(self.api_update_url, data=json.dumps(dv_metadata_params), timeout=self.timeout_seconds)
             print('3) req: %s' % req)
             
             if not req.status_code == 200:
+                
+                print ('request text: %s' % req.text)
+                
                 logger.error('Metadata update failed.  Status code: %s\nResponse:%s' % (req.status_code, req.text))
                 
                 return self.get_result_msg(False, 'Sorry! The update failed.')
