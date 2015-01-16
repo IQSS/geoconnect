15003d3b6584e1aa4e7ba689a57ba915d8afb36e
Raman Prasad <raman_prasad@harvard.edu>
2015-01-16 17:08:46 -0500

    almost done with delete map
    - calls to dataverse and worldmap working outside of the main
    “view_delete_method”
    - write tests, wire up, etc.
    Part of #79

--- geoconnect/apps/gis_shapefiles/views_delete.py
+++ geoconnect/apps/gis_shapefiles/views_delete.py
@@ -7,10 +7,13 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 from apps.worldmap_connect.form_delete import DeleteMapForm
 from apps.dv_notify.metadata_updater import MetadataUpdater
 
-import logging
-logger = logging.getLogger('geoconnect')
+from apps.worldmap_connect.dataverse_layer_services import delete_map_layer
+
 
 def view_delete_map(request):
+    """
+    Attempt to delete a dataverse-created WorldMap layer
+    """
     
     #return HttpResponse('delete')
     if not request.POST:
@@ -22,28 +25,28 @@ def view_delete_map(request):
     
 
     if not f.is_valid():
-
         return HttpResponse('invalid form')
 
     else:
 
-        worldmap_info = f.get_worldmap_layer_info()
-
+        gis_data_file = f.get_gis_data_file_object()
+        worldmap_layer_info = f.get_worldmap_layer_info()
+        
         # -----------------------------------
         # Delete map from WorldMap
         # -----------------------------------
-        
-        #MetadataUpdater.delete_map_metadata_from_dataverse(worldmap_info)
+        delete_map_layer(gis_data_file, worldmap_layer_info)
+        ###  TO DO: HERE
         
         # -----------------------------------
         # Delete metadata from dataverse
         # -----------------------------------
-        (success, err_msg_or_None) = MetadataUpdater.delete_map_metadata_from_dataverse(worldmap_info)
+        (success, err_msg_or_None) = MetadataUpdater.delete_map_metadata_from_dataverse(worldmap_layer_info)
         if not success:
             logger.error("Faild to delete Map Metadata from Dataverse: %s" % err_msg_or_None)
             
         
-        return HttpResponse('%s' % worldmap_info)
+        return HttpResponse('%s' % worldmap_layer_info)
         return HttpResponse('valid form')
 
     """
@@ -55,3 +58,15 @@ def view_delete_map(request):
         logger.error('Shapefile not found for hash: %s' % shp_md5)
         raise Http404('Shapefile not found.')
     """
+'''
+python manage.py shell
+
+from apps.worldmap_connect.dataverse_layer_services import delete_map_layer
+from apps.gis_basic_file.models import GISDataFile
+from apps.worldmap_connect.models import WorldMapLayerInfo
+
+gis_data_file = GISDataFile.objects.get(pk=2)
+worldmap_layer_info = WorldMapLayerInfo.objects.get(pk=6)
+
+delete_map_layer(gis_data_file, worldmap_layer_info)
+'''
--- geoconnect/apps/worldmap_connect/dataverse_layer_services.py
+++ geoconnect/apps/worldmap_connect/dataverse_layer_services.py
@@ -1,8 +1,21 @@
+from __future__ import print_function
+
 import sys
+import logging
+import requests
+
+from django.conf import settings
+
+
+from apps.gis_basic_file.models import GISDataFile
+from apps.worldmap_connect.models import WorldMapLayerInfo
 
 from shared_dataverse_information.dataverse_info.forms import CheckForExistingLayerFormBasic, CheckForDataverseUserLayersFormBasic
+from apps.gis_basic_file.dataverse_info_service import get_dataverse_info_dict
 
-from shared_dataverse_information.worldmap_api_helper.url_helper import GET_LAYER_INFO_BY_USER_AND_FILE_API_PATH, GET_LAYER_INFO_BY_USER_API_PATH
+from shared_dataverse_information.worldmap_api_helper.url_helper import GET_LAYER_INFO_BY_USER_AND_FILE_API_PATH\
+                                                        , GET_LAYER_INFO_BY_USER_API_PATH\
+                                                        , DELETE_LAYER_API_PATH
 
 from geo_utils.message_helper_json import MessageHelperJSON
 
@@ -16,7 +29,48 @@ Functions that interact with the WorldMap API to:
 logger = logging.getLogger(__name__)
 
 
-#def delete_map_layer
+def delete_map_layer(gis_data_file, worldmap_layer_info):
+    
+    assert isinstance(gis_data_file, GISDataFile), "gis_data_file must be a GISDataFile object"
+    assert isinstance(worldmap_layer_info, WorldMapLayerInfo), "worldmap_layer_info must be a WorldMapLayerInfo object"
+    
+    data_params = get_dataverse_info_dict(gis_data_file)
+    data_params[settings.WORLDMAP_TOKEN_NAME_FOR_DV] = settings.WORLDMAP_TOKEN_FOR_DATAVERSE
+    
+
+    try:
+         r = requests.post(DELETE_LAYER_API_PATH\
+                        , data=data_params\
+                        , timeout=30)
+    except requests.exceptions.ConnectionError as e:
+
+        err_msg = """Sorry! Failed to retrieve data from the WorldMap.
+                    <p><b>Details for administrator:</b> Could not contact the
+                    WorldMap server: %s</p><p>%s</p>"""\
+                                % (DELETE_LAYER_API_PATH, e.message)
+        logger.error(err_msg)
+        return MessageHelperJSON.get_dict_msg(success=False, msg=err_msg)
+
+    print (r.text)
+    print (r.status_code)
+
+    #--------------------------------------
+    # Response looks good
+    #--------------------------------------
+    if r.status_code == 200:
+        response_dict = r.json()
+        return MessageHelperJSON.get_dict_msg(success=True, msg='layer returned')
+
+    print (r.text)
+    print (r.status_code)
+    #--------------------------------------
+    # Response doesn't look good
+    #--------------------------------------
+    err_msg = "Status code: %s\nError: %s" % (r.status_code, r.text)
+    return MessageHelperJSON.get_dict_msg(success=False, msg=err_msg)
+    
+    
+
 
 """
 url = 'http://127.0.0.1:8000/dvn-layer/get-dataverse-user-layers/'
@@ -83,5 +137,17 @@ def get_dataverse_layer_info_by_user_and_file(dv_user_id, datafile_id):
         return MessageHelperJSON.get_dict_msg(success=False, msg=err_msg)
             
         
-    
+"""
+python manage.py shell
+
+from apps.worldmap_connect.dataverse_layer_services import delete_map_layer
+from apps.gis_basic_file.models import GISDataFile
+from apps.worldmap_connect.models import WorldMapLayerInfo
+
+gis_data_file = GISDataFile.objects.get(pk=2)
+worldmap_layer_info = WorldMapLayerInfo.objects.get(pk=6)
+
+delete_map_layer(gis_data_file, worldmap_layer_info)
+
+"""    
     
