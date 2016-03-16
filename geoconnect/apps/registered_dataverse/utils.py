8eb6389ace0b289b09e4547e4c2680cb86a6873c
Raman Prasad <raman_prasad@harvard.edu>
2016-03-16 13:53:58 -0400

    bug, wasn't checking for active state
    iqss/dataverse#2934

--- geoconnect/apps/registered_dataverse/utils.py
+++ geoconnect/apps/registered_dataverse/utils.py
@@ -6,7 +6,9 @@ def is_setting_active(setting_slug):
     """
     Check if a setting is active
     """
-    if IncomingFileTypeSetting.objects.filter(slug=setting_slug).count() > 0:
+    filters = dict(slug=setting_slug,
+                    active=True)
+    if IncomingFileTypeSetting.objects.filter(**filters).count() > 0:
         return True
 
     return False
