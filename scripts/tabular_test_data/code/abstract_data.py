ba23eaa21926900c33a50c82e63143d4bb01f962
Raman Prasad <raman_prasad@harvard.edu>
2016-11-28 16:31:21 -0500

    note, used for iqss/dataverse#3477

--- scripts/tabular_test_data/code/abstract_data.py
+++ scripts/tabular_test_data/code/abstract_data.py
@@ -3,8 +3,9 @@ Title, year, doi url, description file
 """
 abstract_info_list = [
 
-('Census Tracts 2010, Boston', 2010, 'http://dx.doi.org/10.7910/DVN/QDRUAZ', 'census_tracts.txt'),
-('Census Block Groups 2010, Boston', 2010, 'http://dx.doi.org/10.7910/DVN/5AF9LK', 'census_block_groups.txt'),
-('Census Blocks 2010, Boston', 2010, 'http://dx.doi.org/10.7910/DVN/FI1YED', 'census_blocks.txt'),
+('Census Tracts 2015, Boston', 2015, 'http://dx.doi.org/10.7910/DVN/QDRUAZ', 'census_tracts.txt'),
+('Census Block Groups 2015, Boston', 2015, 'http://dx.doi.org/10.7910/DVN/5AF9LK', 'census_block_groups.txt'),
+('Census Blocks 2015, Boston', 2015, 'http://dx.doi.org/10.7910/DVN/FI1YED', 'census_blocks.txt'),
+('US Zip Code 2015, Boston', 2015, 'http://dx.doi.org/10.7910/DVN/PJ3JK2', 'zip_codes.txt'),
 
 ]
