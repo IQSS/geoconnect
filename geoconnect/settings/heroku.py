a5d37b2a649293d98ec9a7ba07541b0bef432c78
Raman Prasad <raman_prasad@harvard.edu>
2017-03-15 12:28:59 -0400

    update allowed hosts

--- geoconnect/settings/heroku.py
+++ geoconnect/settings/heroku.py
@@ -104,7 +104,8 @@ SESSION_COOKIE_NAME = 'geoconnect_h1'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = [ os.environ['HEROKU_SERVER_NAME']] # e.g. 'geoconnect-dev.herokuapp.com'
+ALLOWED_HOSTS = [os.environ['HEROKU_SERVER_NAME'],
+                 'geoconnect.datascience.iq.harvard.edu']
 
 ########## LOGIN_URL
 # To use with decorator @login_required
