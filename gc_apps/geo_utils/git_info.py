c1e41b77f08b18c2713b091f95fb0d4379a39def
Raman Prasad <raman_prasad@harvard.edu>
2017-04-14 15:48:18 -0400

    updated

--- gc_apps/geo_utils/git_info.py
+++ gc_apps/geo_utils/git_info.py
@@ -9,6 +9,14 @@ from django.conf import settings
 
 LOGGER = logging.getLogger(__name__)
 
+def get_blank_info_dict():
+    # Return git info dict where all values are None
+    #
+    return dict(git_active_branch=None,
+                git_hexsha=None,
+                git_hexsha_short=None)
+
+
 def get_branch_info():
     """Return the information about this git branch.
     If no branch is present, return an empty dit
@@ -17,11 +25,11 @@ def get_branch_info():
         repo = Repo(settings.SITE_ROOT)
     except (NoSuchPathError, InvalidGitRepositoryError) as ex_obj:
         LOGGER.error('error: %s', ex_obj)
-        return {}
+        return get_blank_info_dict()
 
     repo_commit = repo.commit()
     if not repo_commit:
-        return {}
+        return get_blank_info_dict()
 
     return dict(git_active_branch=repo.active_branch.name,
                 git_hexsha=repo_commit.hexsha,
